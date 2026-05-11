b92b96529fa87bb2745edbadcf157b867da25c36
jep27pr <jep27pr@gmail.com>
2026-05-11 09:18:52 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+{
+  "beppealbertini": {
+    "nome": "Giuseppe",
+    "cognome": "Albertini",
+    "azienda": "OCME srl",
+    "ruolo": "Area Manager",
+    "telefono": "+39 345 0000000",
+    "email": "giuseppe.albertini@ocme.com",
+    "sito": "https://www.ocme.com",
+    "linkedin": "https://www.linkedin.com/in/giuseppealbertini"
+  }
+}
